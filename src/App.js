a2f19823c214b459f11c526e444216a90108146e
Mark <75957685+mbh206@users.noreply.github.com>
2022-11-06 06:53:29 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Header from './Header.js'
 import Main from './Main.js'
 import Footer from './Footer.js'
+import Logo from './mark-with-food.jpg'
 
 
 function App() {
   return (
     <div className="container">
-      <img src="./mark-with-food.jpg" className="main-img" />
+      <img src={Logo} className="main-img" />
       <div className="main-content">
         <Header />
         <Main />
